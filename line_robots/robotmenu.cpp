12437d9e10ca428574aff1447cb97bb44a5a4f1b
Alex Maslar <alexmaslar@Alexs-MacBook-Pro.local>
2020-05-14 13:39:54 -0500

    added robot circle shape to toolbar and removed the old click way of adding a robot

--- line_robots/robotmenu.cpp
+++ line_robots/robotmenu.cpp
@@ -1,12 +1,58 @@
+#include <QtWidgets>
 #include "robotmenu.h"
 
-RobotMenu::RobotMenu(QWidget *parent) :
-    QLabel(parent)
+RobotMenu::RobotMenu(QWidget *parent)
+    : QFrame(parent)
 {
+    // setting up the panel, this does not accept drops
+    setAcceptDrops(false);
 
+    // setting up the left arrow icon
+    QLabel *robotCircle = new QLabel(this);
+    QPixmap circle(":/images/circle.png");
+    const int w = 30;
+    const int h = 30;
+
+    robotCircle->setPixmap(circle.scaled(w, h, Qt::IgnoreAspectRatio));
+    robotCircle->setToolTip("Circle Robot");		// DON'T CHANGE - used to create robot object
+    robotCircle->move(8, 8);
+    robotCircle->show();
+    robotCircle->setAttribute(Qt::WA_DeleteOnClose);
 }
 
-void RobotMenu::mousePressEvent(QMouseEvent *ev)
+
+void RobotMenu::mousePressEvent(QMouseEvent *event)
 {
-    emit Mouse_Pressed();
+    QLabel *child = static_cast<QLabel *>(childAt(event->pos()));
+    if (!child)
+        return;
+
+    QPixmap pixmap = *child->pixmap();
+
+    QByteArray itemData;
+    QDataStream dataStream(&itemData, QIODevice::WriteOnly);
+    dataStream << pixmap << QPoint(event->pos() - child->pos());
+
+    QMimeData *mimeData = new QMimeData;
+    mimeData->setData("application/x-dnditemdata", itemData);
+    mimeData->setText(child->toolTip());
+
+    QDrag *drag = new QDrag(this);
+    drag->setMimeData(mimeData);
+    drag->setPixmap(pixmap);
+    drag->setHotSpot(event->pos() - child->pos());
+
+    QPixmap tempPixmap = pixmap;
+    QPainter painter;
+    painter.begin(&tempPixmap);
+    painter.fillRect(pixmap.rect(), QColor(127, 127, 127, 127));
+
+    child->setPixmap(tempPixmap);
+
+    if (drag->exec(Qt::CopyAction | Qt::MoveAction, Qt::CopyAction) == Qt::MoveAction) {
+        child->close();
+    } else {
+        child->show();
+        child->setPixmap(pixmap);
+    }
 }
