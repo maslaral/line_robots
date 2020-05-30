d5547459e03ccd12661c609c845303b77c517ac7
halsam <wchauks@gmail.com>
2020-05-30 17:16:13 -0400

    added menu bar
    replaced buttons for a menu bar
    
    also added confirmation for exititing the program and clearing the canvas

--- line_robots/mainwindow.cpp
+++ line_robots/mainwindow.cpp
@@ -12,6 +12,9 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     ui->setupUi(this);
     scene = new Canvas(itemMenu, this);
 
+    createActions();
+    createMenus();
+
     // Sets the size of the canvas
     scene->setSceneRect(0, 0, 554, 466);
 
@@ -32,14 +35,35 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     connect(ui->speed, SIGNAL(valueChanged(int)), timer, SLOT(setFrameRate(int)));
     connect(timer, SIGNAL(isRunning(bool)), ui->lineMenu, SLOT(setDisabled(bool)));
     connect(timer, SIGNAL(isRunning(bool)), ui->robotMenu, SLOT(setDisabled(bool)));
-    connect(timer, SIGNAL(isRunning(bool)), ui->butUndo, SLOT(setDisabled(bool)));
-    connect(timer, SIGNAL(isRunning(bool)), ui->butRedo, SLOT(setDisabled(bool)));
+    connect(timer, SIGNAL(isRunning(bool)), toolsMenu, SLOT(setDisabled(bool)));
 }
 
 void MainWindow::clearData()
 {
     scene->clear();
     scene->undoStack->clear();
+}
+
+bool MainWindow::confirmAction(int action)
+{
+    QMessageBox msgBox;
+    msgBox.setStandardButtons(QMessageBox::Yes);
+    msgBox.addButton(QMessageBox::No);
+    switch(action){
+    case 0:
+        msgBox.setText("Are you sure you want to quit?");
+        if(msgBox.exec() == QMessageBox::Yes){
+            return true;
+        }
+        break;
+    case 1:
+        msgBox.setText("Are you sure you want to PERMANENTLY delete all objects?");
+        if(msgBox.exec() == QMessageBox::Yes){
+            return true;
+        }
+        break;
+    }
+    return false;
 }
 
 MainWindow::~MainWindow()
@@ -58,21 +82,62 @@ void MainWindow::setPause(bool timerActive)
     ui->pauseButton->repaint(); // required so text visibly updates
 }
 
-void MainWindow::on_butClear_clicked()
+void MainWindow::exit()
 {
-    clearData();
-    this->timer->stop();
-    this->setPause(false);
-    ui->lineMenu->setEnabled(true);
-    ui->robotMenu->setEnabled(true);
+    bool tmp = confirmAction(0);
+    if(tmp == true){
+        this->close();
+    }
 }
 
-void MainWindow::on_butUndo_clicked()
+void MainWindow::undo()
 {
     scene->undoStack->undo();
 }
 
-void MainWindow::on_butRedo_clicked()
+void MainWindow::redo()
 {
     scene->undoStack->redo();
+}
+
+void MainWindow::clear()
+{
+    bool tmp = confirmAction(1);
+    if(tmp == true){
+        clearData();
+        this->timer->stop();
+        this->setPause(false);
+        ui->lineMenu->setEnabled(true);
+        ui->robotMenu->setEnabled(true);
+    }
+}
+
+void MainWindow::createMenus()
+{
+    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu->addAction(exitAct);
+
+    toolsMenu = menuBar()->addMenu(tr("&Tools"));
+    toolsMenu->addAction(undoAct);
+    toolsMenu->addAction(redoAct);
+    toolsMenu->addSeparator();
+    toolsMenu->addAction(clearAct);
+}
+
+void MainWindow::createActions()
+{
+    exitAct = new QAction(tr("&Exit"), this);
+    connect(exitAct, &QAction::triggered, this, &MainWindow::exit);
+
+    undoAct = new QAction(tr("&Undo"), this);
+    undoAct->setStatusTip("Undo latest addition to the canvas");
+    connect(undoAct, &QAction::triggered, this, &MainWindow::undo);
+
+    redoAct = new QAction(tr("&Redo"), this);
+    redoAct->setStatusTip("Redo latest addition to the canvas");
+    connect(redoAct, &QAction::triggered, this, &MainWindow::redo);
+
+    clearAct = new QAction(tr("&Clear"), this);
+    clearAct->setStatusTip("Permanently remove all objects from the canvas");
+    connect(clearAct, &QAction::triggered, this, &MainWindow::clear);
 }
